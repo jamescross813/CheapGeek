0540888370016469c11763bfb98a6b7d1cca8246
jamescross <crossjames813@gmail.com>
2021-02-25 15:13:30 -0600

    Game class basic setup

--- lib/CheapGeek/game.rb
+++ lib/CheapGeek/game.rb
@@ -1,14 +1,25 @@
 require 'pry'
  
-attr_accessor :title, :genre, :description, :release_date, :game_url
+
 
 class Game
     
+    attr_accessor :title, :genre, :description, :release_date, :game_url
+
+    @@all_games = []
+
     def intialize(title, genre, short_description, release_date, game_url)
     @title = title
     @genre = genre
     @description = short_description
     @release_date = release_date
     @game_url = game_url
+    @@all_games << self
     end
+
+    def elf.all_games
+        @@all_games
+    end
+
+
 end
