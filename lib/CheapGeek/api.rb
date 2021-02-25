558b66650d2ae4eae17159404322446a51378541
jamescross <crossjames813@gmail.com>
2021-02-25 13:15:43 -0600

    setting up API

--- lib/CheapGeek/api.rb
+++ lib/CheapGeek/api.rb
@@ -1,3 +1,16 @@
+require 'httparty'
+require 'pry'
+ 
+
 class API
+   URL = "https://freetogame.com/api/games"
+
+   def  self.get_info
+    value = HTTParty.get(URL)
+  end
+
+  
 
 end
+
+#binding.pry
--- lib/CheapGeek/cli.rb
+++ lib/CheapGeek/cli.rb
@@ -1,3 +1,5 @@
+require 'pry'
+ 
 class CLI
 
 end
