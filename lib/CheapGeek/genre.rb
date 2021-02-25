b2002b915158bd886626acecf8f2c49b9f1d0b79
jamescross <crossjames813@gmail.com>
2021-02-25 15:04:56 -0600

    Genre class basics

--- lib/CheapGeek/genre.rb
+++ lib/CheapGeek/genre.rb
@@ -2,6 +2,18 @@ require 'pry'
  
 class Category
 
+attr_accessor :genre_name, :game_title
 
-    
+@@all = []
+
+    def initialize(genre, title)
+        @genre_name = genre
+        @game_title = title
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
 end
