6c5785f8d1ef0f3bf28687bb731b617362262600
jamescross <crossjames813@gmail.com>
2021-02-25 13:21:46 -0600

    initialize method and attr for Game class

--- lib/CheapGeek/game.rb
+++ lib/CheapGeek/game.rb
@@ -1,5 +1,14 @@
 require 'pry'
  
-class Game
+attr_accessor :title, :genre, :description, :release_date, :game_url
 
+class Game
+    
+    def intialize(title, genre, short_description, release_date, game_url)
+    @title = title
+    @genre = genre
+    @description = short_description
+    @release_date = release_date
+    @game_url = game_url
+    end
 end
