8355d28cc1a8b3ddfe0eb7ea6215a122953c3fa6
jamescross <crossjames813@gmail.com>
2021-02-25 18:26:26 -0600

    Environment setup

---
+++ config/environment.rb
@@ -0,0 +1,13 @@
+#
+
+require "pry"
+require "awesome_print"
+require "open-uri"
+require "httparty"
+require "rsepc"
+
+require_relative 'CheapGeek/version'
+require_relative '../lib/CheapGeek/api.rb'
+require_relative '../lib/CheapGeek/cli.rb'
+require_relative '../lib/CheapGeek/game.rb'
+require_relative '../lib/CheapGeek/catergory.rb'
