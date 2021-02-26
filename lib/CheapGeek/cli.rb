6166e915acc5a8d3091ae3a749dfbb96c1d26768
jamescross <crossjames813@gmail.com>
2021-02-26 17:08:51 -0600

    CLI set up

--- lib/CheapGeek/cli.rb
+++ lib/CheapGeek/cli.rb
@@ -1,6 +1,10 @@
 require 'pry'
  
 class CLI
+
+    def call 
+        puts "It's dangerous to go alone! What do you desire?"
+    end
 #  start with welcome
 # ask for selection of genre
 # return list of games with that catagory
