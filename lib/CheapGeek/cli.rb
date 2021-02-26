66d984f8b9fc75e59fa8c27bd060a8753f11f292
jamescross <crossjames813@gmail.com>
2021-02-26 17:14:05 -0600

    update require paths

--- lib/CheapGeek/cli.rb
+++ lib/CheapGeek/cli.rb
@@ -2,11 +2,15 @@ require 'pry'
  
 class CLI
 
+
+    #  start with welcome
     def call 
         puts "It's dangerous to go alone! What do you desire?"
     end
-#  start with welcome
+
 # ask for selection of genre
+
+
 # return list of games with that catagory
 # chose game from returned list
 # return information about requested game
--- lib/cheapgeek.rb
+++ lib/cheapgeek.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-
+require_relative './CheapGeek/version'
 
 module CheapGeek
   class Error < StandardError; end
