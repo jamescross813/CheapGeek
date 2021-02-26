ff48cf874260ba25ebe76bc91c8e3c136de5a706
jamescross <crossjames813@gmail.com>
2021-02-26 17:22:11 -0600

    config enviroment

--- config/environment.rb
+++ config/environment.rb
@@ -6,8 +6,8 @@ require "open-uri"
 require "httparty"
 require "rsepc"
 
-require_relative './CheapGeek/version.rb'
-require_relative '../lib/CheapGeek/api.rb'
+require_relative './CheapGeek/version'
+# require_relative '../lib/CheapGeek/api.rb'
 require_relative '../lib/CheapGeek/cli.rb'
-require_relative '../lib/CheapGeek/game.rb'
-require_relative '../lib/CheapGeek/catergory.rb'
+# require_relative '../lib/CheapGeek/game.rb'
+# require_relative '../lib/CheapGeek/catergory.rb'
