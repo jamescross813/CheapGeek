f2830d450a52d02a18456d03ddcc66c1cb577854
jamescross <crossjames813@gmail.com>
2021-02-26 17:18:12 -0600

    pathway set up and tidy up of enviroment files

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,4 @@
-#
+
 
 require "pry"
 require "awesome_print"
@@ -6,7 +6,7 @@ require "open-uri"
 require "httparty"
 require "rsepc"
 
-require_relative 'CheapGeek/version'
+require_relative './CheapGeek/version.rb'
 require_relative '../lib/CheapGeek/api.rb'
 require_relative '../lib/CheapGeek/cli.rb'
 require_relative '../lib/CheapGeek/game.rb'
--- lib/cheapgeek.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-require_relative './CheapGeek/version'
-
-module CheapGeek
-  class Error < StandardError; end
-  # Your code goes here...
-end
