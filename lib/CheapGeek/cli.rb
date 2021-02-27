a5ea0a61d21c61efe4d7535791d0455bfb96d923
jamescross <crossjames813@gmail.com>
2021-02-26 19:22:24 -0600

    CLI basic coding completed including outlines of methods

--- lib/CheapGeek/cli.rb
+++ lib/CheapGeek/cli.rb
@@ -5,10 +5,50 @@ class CLI
 
     #  start with welcome
     def call 
-        puts "It's dangerous to go alone! What do you desire?"
+        puts "It's dangerous to go alone!"
+        genre_list
+        genre_menu
+        game_list
+        game_menu
+        end_app
     end
 
+    def genre_list
+
+    end
 # ask for selection of genre
+    def genre_menu
+        puts "What do you what do you seek?"
+        if input != "exit"
+            input= gets.strip
+            case input
+            when "1"
+                puts "What do you desire?"
+                game_list.genre(1)
+            end
+        end
+    end
+
+    def game_list
+
+    end
+
+    def game_menu
+        puts "There is no Earthly way of knowing...Unless you select the game."
+        if input != "exit"
+            input= gets.strip
+            case input
+            when "1"
+                puts "After very careful consideration, sir, you've come to a decision about #{game_name}"
+                game.name(1)
+            end
+        end
+    end
+
+    def end_app
+        puts "If I don't see you around, I'll see you square."
+    end
+
 
 
 # return list of games with that catagory
