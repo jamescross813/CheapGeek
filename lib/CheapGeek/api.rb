e125c5eb7d46e3621e9fa2cdfad37e25c76f1533
jamescross <crossjames813@gmail.com>
2021-02-26 21:10:24 -0600

    net/http added to gem required list for api

--- lib/CheapGeek/api.rb
+++ lib/CheapGeek/api.rb
@@ -8,14 +8,14 @@ require_relative './genre'
  
 
 class API
-   #URL = "https://freetogame.com/api/games"
+   URL = "https://freetogame.com/api/games"
 
    def self.get_info(url)
-    response = HTTParty.get(url)
+    response = HTTParty.get(URL)
     
     
      info = JSON.parse(response.body)
-     #binding.pry
+     binding.pry
     info.collect do |data|
         new_game = Game.new
         new_game.title = data["title"]
