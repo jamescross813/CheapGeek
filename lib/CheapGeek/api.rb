911311d00a883fe66a8484484bace11b417fdff5
jamescross <crossjames813@gmail.com>
2021-02-27 13:11:07 -0600

    WIP to pass information into objects

--- lib/CheapGeek/api.rb
+++ lib/CheapGeek/api.rb
@@ -1,18 +1,22 @@
 #!/usr/bin/env ruby
 require 'pry'
+require 'httparty'
 class API
    URL = "https://www.freetogame.com/api/games"
-  
+   
+   
    def self.get_info(url)
     response = HTTParty.get(URL)
     info = JSON.parse(response.body)  
+    
     info.each do |data|  
+       
         Game.new(data["title"], 
                 data["genre"], 
                 data["short_description"], 
                 data ["release_date"], 
                 data["game_url"])
-        # Genre.new(data["genre"], data["title"])
+        Genre.new(data["genre"], data["title"])
         end   
     end
 
@@ -20,4 +24,3 @@ end
 
 
 
-#binding.pry
