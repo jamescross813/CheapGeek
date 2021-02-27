151d68dfbf0dee1808ba6a7d89a477fd48727545
jamescross <crossjames813@gmail.com>
2021-02-26 20:47:47 -0600

    update required gems

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,7 @@ require "awesome_print"
 require "open-uri"
 require "httparty"
 require "rspec"
+require "tty-prompt"
 
 require_relative '../lib/CheapGeek/version'
 require_relative '../lib/CheapGeek/api'
--- lib/CheapGeek/cli.rb
+++ lib/CheapGeek/cli.rb
@@ -19,20 +19,7 @@ class CLI
     end
 # ask for selection of genre
     def genre_menu 
-        input = nil
-        while input != "exit"
-            puts "What do  you seek?"
-            input= gets.strip
-            case input
-            when "1"
-                puts "What do you desire?"
-                game_list
-            when "list"
-                genre_list
-            else
-                puts "Button mashing error! Try again!"
-            end
-        end
+            prompt.select("What do  you seek?", genre_list)
     end
 # return list of games with that catagory
     def game_list
@@ -40,20 +27,7 @@ class CLI
     end
 # chose game from returned list
     def game_menu
-        input = nil
-        while input != "exit"
-            puts "There is no Earthly way of knowing...Unless you select the game."
-            input= gets.strip
-            case input
-            when "1"
-                puts "After very careful consideration, sir, you've come to a decision about #{game_name}"
-                game_info.name(1)
-            when "list"
-                genre_list
-            else
-                puts "Button mashing error! Try again!"
-            end
-        end
+        prompt.select("What do you desire?", game_list)
     end
 # return information about requested game
     def game_info
