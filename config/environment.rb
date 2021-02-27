a83da977a68908bd97ea635179de31ee10bae02b
jamescross <crossjames813@gmail.com>
2021-02-27 12:49:27 -0600

    clean up of redundant gems

--- config/environment.rb
+++ config/environment.rb
@@ -4,10 +4,10 @@ require "open-uri"
 require "httparty"
 require "rspec"
 require "tty-prompt"
-require "net/http"
+
 
 require_relative '../lib/CheapGeek/version'
 require_relative '../lib/CheapGeek/api'
-require_relative '../lib/CheapGeek/cli'
+# require_relative '../lib/CheapGeek/cli'
 require_relative '../lib/CheapGeek/game'
 require_relative '../lib/CheapGeek/genre'
--- lib/CheapGeek/api.rb
+++ lib/CheapGeek/api.rb
@@ -1,26 +1,18 @@
-require 'httparty'
+#!/usr/bin/env ruby
 require 'pry'
-require 'net/http'
-require 'open-uri'
-require 'json'
-require_relative './game'
-require_relative './genre'
- 
-
 class API
-   URL = "https://freetogame.com/api/games"
-
+   URL = "https://www.freetogame.com/api/games"
+  
    def self.get_info(url)
     response = HTTParty.get(URL)
-       info = JSON.parse(response.body)
-    info.each do |data|
+    info = JSON.parse(response.body)  
+    info.each do |data|  
         Game.new(data["title"], 
                 data["genre"], 
                 data["short_description"], 
                 data ["release_date"], 
                 data["game_url"])
-        Genre.new(data["genre"], data["title"])
-        binding.pry
+        # Genre.new(data["genre"], data["title"])
         end   
     end
 
