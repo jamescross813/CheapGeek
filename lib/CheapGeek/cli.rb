de6680639ca3ed5d0df8226bb6ee0aa20365b966
jamescross <crossjames813@gmail.com>
2021-02-28 22:06:19 -0600

    working cycle between choice mae and waitin_menu, sleep code included

--- lib/CheapGeek/cli.rb
+++ lib/CheapGeek/cli.rb
@@ -9,9 +9,6 @@ class CLI
         # genre_list
         # genre_menu
         game_list
-        game_menu
-        game_info
-        # end_app
     end
 
     # def genre_list
@@ -31,6 +28,7 @@ class CLI
 
     def game_list
         @game_list = Game.all_games
+        game_menu
     end
 
 # chose game from returned list
@@ -39,22 +37,31 @@ class CLI
         @game_list.each {|game| @game_menu << game.title}
         prompt = TTY::Prompt.new
         @choice = prompt.select("What do you desire?", @game_menu, cycle: true, symbols: { marker: ">" }, filter: true)
+        sleep (1)
+        game_info
     end
 
 # return information about requested game
     def game_info
         @game_list.each do |game| 
             if game.title == @choice
-               
                @info = "#{game.short_description} #{game.release_date} #{game.game_url}" 
-               #binding.pry
                puts @info
+               sleep (5)
+               waiting_menu
             end
-            
         end
-            
-        
-     
+    end
+
+    def waiting_menu
+        @waiting_menu = ["Return to games.", "Exit, pursued by bear."]
+        prompt = TTY::Prompt.new
+        @choice = prompt.select("Try and try again, right?!", @waiting_menu, cycle: true, symbols: { marker: ">" })
+        if @choice == @waiting_menu[0]
+            game_menu
+        else
+            end_app
+        end
     end
 
     def end_app
